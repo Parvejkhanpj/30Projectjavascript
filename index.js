cf89f4941501323f87e1b7f75f41e51462a575e9
Parvej khan <kparvej761@gmail.com>
2022-11-06 21:58:33 +0530

    onclick sound feature

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 console.log("helo drum Kit");
-window.addEventListener("keydown", function (e) {
+
+// playting sound funtion
+function playStation(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
   key.classList.add("playing");
-});
+}
 
 const keys = document.querySelectorAll(".key");
-
+//  remove transition effect
 function removeTransition(e) {
   if (e.propertyName !== "transform") return; // skip it if it's is not transform
   this.classList.remove("playing");
-  console.log(e.propertyName);
 }
+
+// onclick effecţ
+
+let numberofKeys = document.querySelectorAll(".key").length;
+for (let i = 0; i < numberofKeys; i++) {
+  document.querySelectorAll(".key")[i].addEventListener("click", function (e) {
+    let keyValue = keys[i].getAttribute("data-key");
+    const audio = document.querySelector(`audio[data-key="${keyValue}"]`);
+    const key = document.querySelector(`.key[data-key="${keyValue}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add("playing");
+  });
+}
+
+// remove transioton event handler
 keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+window.addEventListener("keydown", playStation);
